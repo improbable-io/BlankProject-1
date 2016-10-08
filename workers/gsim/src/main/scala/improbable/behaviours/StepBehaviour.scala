1d01725c7cc801e9d5cb93d93b4599f2d4d4fc21
apuchitnis <apuchitnis@hotmail.com>
2016-10-08 17:15:17 +0100

    ..

---
+++ workers/gsim/src/main/scala/improbable/behaviours/StepBehaviour.scala
@@ -0,0 +1,10 @@
+import improbable.papi.entity.{Entity, EntityBehaviour}
+import improbable.papi.world.World
+import improbable.logging.Logger
+
+class StepBehaviour(entity: Entity, world: World, logger: Logger) extends EntityBehaviour {
+
+  override def onReady(): Unit = {
+
+  }
+}
